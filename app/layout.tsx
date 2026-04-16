7adc814af151378d576ecc68871ee5d96e3b4240
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-04-16 14:03:18 +0300

    refactor project structure and add component folders

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import TanStackProvider from '@/components/providers/TanStackProvider/TanStackProvider';
 import './globals.css';
 
 export const metadata: Metadata = {
