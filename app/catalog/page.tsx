c075347c980e64b55de4bc4db2f50613bde5214f
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-04-17 22:11:52 +0300

    feat: add catalog layout with sidebar filters

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -10,7 +10,66 @@ export default async function CatalogPage() {
   return (
     <main className="container">
       <div className={styles.layout}>
-        <aside className={styles.sidebar}>Sidebar</aside>
+        <aside className={styles.sidebar}>
+          <div className={styles.filtersBox}>
+            <p className={styles.locationLabel}>Location</p>
+
+            <div className={styles.locationInput}>Kyiv, Ukraine</div>
+
+            <p className={styles.filtersTitle}>Filters</p>
+
+            <div className={styles.filterGroup}>
+              <p className={styles.groupTitle}>Camper form</p>
+
+              <label className={styles.option}>
+                <input type="radio" name="form" /> Alcove
+              </label>
+              <label className={styles.option}>
+                <input type="radio" name="form" /> Panel Van
+              </label>
+              <label className={styles.option}>
+                <input type="radio" name="form" /> Integrated
+              </label>
+              <label className={styles.option}>
+                <input type="radio" name="form" /> Semi Integrated
+              </label>
+            </div>
+
+            <div className={styles.filterGroup}>
+              <p className={styles.groupTitle}>Engine</p>
+
+              <label className={styles.option}>
+                <input type="radio" name="engine" /> Diesel
+              </label>
+              <label className={styles.option}>
+                <input type="radio" name="engine" /> Petrol
+              </label>
+              <label className={styles.option}>
+                <input type="radio" name="engine" /> Hybrid
+              </label>
+              <label className={styles.option}>
+                <input type="radio" name="engine" /> Electric
+              </label>
+            </div>
+
+            <div className={styles.filterGroup}>
+              <p className={styles.groupTitle}>Transmission</p>
+
+              <label className={styles.option}>
+                <input type="radio" name="transmission" /> Automatic
+              </label>
+              <label className={styles.option}>
+                <input type="radio" name="transmission" /> Manual
+              </label>
+            </div>
+            <Button variant="filled" className={styles.searchBtn}>
+              Search
+            </Button>
+            <Button variant="outlined" className={styles.clearBtn}>
+              Clear filters
+            </Button>
+          </div>
+        </aside>
         <div className={styles.content}>
           <ul className={styles.list}>
             {campers.items.map((camper: Camper, index: number) => (
