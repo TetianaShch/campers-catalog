bd6c12553fbff9a139e84bc0d26b869ec0698ac5
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-04-19 23:25:14 +0300

    feat: add styles for camper details info and specs

--- components/camper/VehicleDetails/VehicleDetails.tsx
+++ components/camper/VehicleDetails/VehicleDetails.tsx
@@ -1,3 +1,5 @@
+import styles from './VehicleDetails.module.css';
+
 type Props = {
   form: string;
   length: string;
@@ -16,16 +18,47 @@ export default function CamperSpecs({
   consumption,
 }: Props) {
   return (
-    <div>
-      <h3>Vehicle details</h3>
+    <div className={styles.wrapper}>
+      <h3 className={styles.title}>Vehicle details</h3>
 
-      <ul>
-        <li>Form: {form}</li>
-        <li>Length: {length}</li>
-        <li>Width: {width}</li>
-        <li>Height: {height}</li>
-        <li>Tank: {tank}</li>
-        <li>Consumption: {consumption}</li>
+      {/* ЧІПСИ */}
+      <div className={styles.features}>
+        <span className={styles.tag}>Automatic</span>
+        <span className={styles.tag}>AC</span>
+        <span className={styles.tag}>Petrol</span>
+        <span className={styles.tag}>Kitchen</span>
+        <span className={styles.tag}>Radio</span>
+        <span className={styles.tag}>Alcove</span>
+      </div>
+
+      {/* ЛІНІЯ */}
+      <div className={styles.divider} />
+
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <span>Form</span>
+          <span>{form}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Length</span>
+          <span>{length}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Width</span>
+          <span>{width}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Height</span>
+          <span>{height}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Tank</span>
+          <span>{tank}</span>
+        </li>
+        <li className={styles.item}>
+          <span>Consumption</span>
+          <span>{consumption}</span>
+        </li>
       </ul>
     </div>
   );
